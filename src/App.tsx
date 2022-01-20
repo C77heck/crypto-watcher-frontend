67697bcf717a40d4fb69623af31a1e9e46facf51
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-01-20 19:25:14 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import { Spinner } from './shared/components/spinner';
 
 function App() {
+  const routes = (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <h2>suck it bitch</h2>
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='center'>
+      <React.Suspense
+        fallback={<div><Spinner overlay /></div>}
+      >
+        {routes}
+      </React.Suspense>
+    </main>
   );
 }
 
---
+++ src/shared/components/spinner.tsx
@@ -0,0 +1,6 @@
+
+import * as React from 'react';
+
+export const Spinner = (props: any) => {
+    return <h2>
+}
