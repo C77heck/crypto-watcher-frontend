596fdba4a9c5322a22f8a24ce8afa5172cc626ea
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-02-07 14:03:26 +0100

    wip

--- src/shared/form/validators/only-strings-validator.ts
+++ src/shared/form/validators/only-strings-validator.ts
@@ -1,11 +1,9 @@
 export const onlyStringsValidator = (value: any) => {
     if (!value) {
-        console.log({ value: !value });
         return true;
     }
 
     const hasOnlyStrings = /\D/;
-    console.log({ value: hasOnlyStrings.test(value) });
-
+    
     return hasOnlyStrings.test(value);
 };
