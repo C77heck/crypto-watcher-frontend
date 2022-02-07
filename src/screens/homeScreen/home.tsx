bce39e07abe6c00fde65dc22ffce8f4c875e0024
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-02-07 07:57:48 +0100

    wip

--- src/screens/homeScreen/home.tsx
+++ src/screens/homeScreen/home.tsx
@@ -1,10 +1,14 @@
-import { Input } from "../../shared/form/input"
-import { Header } from "../components/header"
+import { Input } from "../../shared/form/input";
+import { Header } from "../components/header";
 
 export const Home = (props: any) => {
 
     return <div>
-        <Header />
-        <Input />
-    </div>
-}
+        <Header/>
+        <Input
+            validator={() => false}
+            getData={(data: any) => console.log(data)}
+            errorMessage={'whaaat?'}
+        />
+    </div>;
+};
--- src/shared/form/input.tsx
+++ src/shared/form/input.tsx
@@ -1,7 +1,7 @@
 import { Component } from "react";
 
 export class Input extends Component<any, any> {
-    public state = { value: '' };
+    public state = { value: '', errors: [] };
     public values: any[] = [];
 
     public componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<any>, snapshot?: any) {
@@ -10,24 +10,41 @@ export class Input extends Component<any, any> {
         }
     }
 
+    public validate(value: string) {
+        return !!this.props.validator ? this.props.validator(value) : true;
+    }
+
     public handleChange({ target: { value } }: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({ value });
+        this.setState({ value, errors: this.validate(value) });
+    }
+
+    public getData() {
+        if (this.props.getData) {
+            this.props.getData(this.state.value, this.state.errors);
+        }
     }
 
     public render() {
-        return <div className={'input-wrapper'}>
-            <input
-                className={'input'}
-                onChange={(e) => this.handleChange(e)} value={this.state.value}
-                type={this.props.type || 'text'}
-                name={this.props.name}
-                id={this.props.id}
-                readOnly={this.props.readOnly}
-                required={this.props.required}
-                placeholder={this.props.placeholder}
-                autoComplete={this.props.autoComplete}
-                disabled={this.props.disabled}
-            />
+        const hasError = true; // !!this.state.errors.length;
+        return <div className={'display-flex flex-column'}>
+            <div
+                className={`input-wrapper ${this.props.classNames} error-${hasError ? 'show' : 'hide'}`}
+            >
+                <input
+                    className={'input'}
+                    onChange={(e) => this.handleChange(e)}
+                    value={this.state.value}
+                    type={this.props.type || 'text'}
+                    name={this.props.name}
+                    id={this.props.id}
+                    readOnly={this.props.readOnly}
+                    required={this.props.required}
+                    placeholder={this.props.placeholder}
+                    autoComplete={this.props.autoComplete}
+                    disabled={this.props.disabled}
+                />
+            </div>
+            {!!hasError && <small className={'error-show'}>{this.props.errorMessage}</small>}
         </div>;
     }
 }
