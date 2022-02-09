6c0585756a6eb9283fbd9cc8238a32778910b68d
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-02-09 08:10:56 +0100

    wip

--- src/screens/newCrypto/components/new-crypto.form.tsx
+++ src/screens/newCrypto/components/new-crypto.form.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
+import { Button } from '../../../shared/components/button';
 import { Input } from '../../../shared/form/input';
 import { onlyStringsValidator } from '../../../shared/form/validators/only-strings-validator';
 
 export const NewCryptoForm = (props: any) => {
     const [form, setForm] = useState({});
     const getData = (data: any, isValid: any) => {
-        setForm({ [data?.name]: data?.value, ...form });
+        console.log(data);
+        setForm({ ...form, [data?.name]: { value: data?.value, isValid: isValid } });
+
     };
     const config = {
         first: {
@@ -31,40 +34,39 @@ export const NewCryptoForm = (props: any) => {
     };
 
     const submit = (e: any) => {
+        e.preventDefault();
         console.log({ e, form });
     };
 
-    return <form onSubmit={() => console.log('attempt at submitting.')}>
+    return <form onSubmit={(e) => submit(e)}>
         <Input
             {...config.first}
             validator={(data: any) => onlyStringsValidator(data)}
-            getData={(data: any, error: any) => console.log(data, error)}
             errorMessage={'whaaat?'}
         />
         <Input
             {...config.second}
             validator={(data: any) => onlyStringsValidator(data)}
-            getData={(data: any, error: any) => console.log(data, error)}
             errorMessage={'whaaat?'}
         />
         <Input
             {...config.third}
             validator={(data: any) => onlyStringsValidator(data)}
-            getData={(data: any, error: any) => console.log(data, error)}
             errorMessage={'whaaat?'}
         />
         <Input
             {...config.fourth}
             validator={(data: any) => onlyStringsValidator(data)}
-            getData={(data: any, error: any) => console.log(data, error)}
             errorMessage={'whaaat?'}
         />
         <Input
             {...config.fifth}
             validator={(data: any) => onlyStringsValidator(data)}
-            getData={(data: any, error: any) => console.log(data, error)}
             errorMessage={'whaaat?'}
         />
-
+        <Button
+            title={'Go'}
+            type={'submit'}
+        />
     </form>;
 };
--- src/shared/components/button.tsx
+++ src/shared/components/button.tsx
@@ -1,9 +1,23 @@
 export const Button = (props: any) => {
-
     return <button
-        type={props.type}
-        className={props.className}
+        type={props.type || 'button'}
+        className={getButtonType(props.className)}
+        name={props.name}
+        id={props.id}
+        disabled={props.disabled}
+        onClick={props.onClick}
     >
         {!!props.children ? props.children : props.title}
     </button>;
+};
+
+const getButtonType = (type: string) => {
+    switch (type) {
+        case 'base':
+            return 'button button--base';
+        case 'submit':
+            return 'button button--submit';
+        default:
+            return 'button';
+    }
 };
