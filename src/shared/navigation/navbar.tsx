6c3efb0b4c55af2ec1c9d0186f0c1e3646f60e4c
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-02-22 07:32:29 +0100

    wip

--- src/shared/navigation/navbar.tsx
+++ src/shared/navigation/navbar.tsx
@@ -4,6 +4,10 @@ import { staticData } from '../config/static-data';
 import { LatestListings } from './latest-listings';
 import { LoginButton } from './login.button';
 
+const getColor = (link: string) => {
+    return window.location.pathname === link ? 'text-color--active' : '';
+};
+
 export const NavBar = (props: any) => {
     const { links: { watchlist, newPurchase, profitCalculator } } = staticData;
 
@@ -11,9 +15,30 @@ export const NavBar = (props: any) => {
         <nav className="nav-bar position-center">
             <div className={'row max-width-vw-85'}>
                 <ul className="nav-bar--ul col-60">
-                    <li><Link to={watchlist}>watchlist</Link></li>
-                    <li><Link to={newPurchase}>new purchase</Link></li>
-                    <li><Link to={profitCalculator}>profit calculator</Link></li>
+                    <li>
+                        <Link
+                            className={getColor(watchlist)}
+                            to={watchlist}
+                        >
+                            watchlist
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className={getColor(newPurchase)}
+                            to={newPurchase}
+                        >
+                            new purchase
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className={getColor(profitCalculator)}
+                            to={profitCalculator}
+                        >
+                            profit calculator
+                        </Link>
+                    </li>
                     <li><LatestListings/></li>
                 </ul>
                 <div className={'col-14'}>
