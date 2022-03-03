464fc7da8088873930d41094a96ba555ae25bf46
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-03-03 08:14:41 +0100

    wip

--- src/shared/navigation/mobile-links.tsx
+++ src/shared/navigation/mobile-links.tsx
@@ -10,31 +10,57 @@ export const MobileLinks = (props: any) => {
     }, []);
 
     const isLoggedIn = props;
-    return <ul className="nav-bar--ul row">
-        <li className={'col-20'}>
-            <Link className={getColor(home)} to={home}>
-                Home
-            </Link>
-        </li>
-        {isLoggedIn && <li className={'col-20'}>
-            <Link className={getColor(watchlist)} to={watchlist}>
-                watchlist
-            </Link>
-        </li>}
-        {isLoggedIn && <li className={'col-20'}>
-            <Link className={getColor(newPurchase)} to={newPurchase}>
-                new purchase
-            </Link>
-        </li>}
-        <li className={'col-20'}>
-            <Link className={getColor(changesInValue)} to={changesInValue}>
-                Fluctuation
-            </Link>
-        </li>
-        {isLoggedIn && <li className={'col-20'}>
-            <Link className={getColor(favourites)} to={favourites}>
-                Favourites
-            </Link>
-        </li>}
-    </ul>;
+    return <div className={'mobile-navbar'}>
+        <ul className="nav-bar--ul row">
+            <li className={'col-20'}>
+                <Link className={getColor(home)} to={home}>
+                    Home
+                </Link>
+            </li>
+            {isLoggedIn && <li className={'col-20'}>
+                <Link className={getColor(watchlist)} to={watchlist}>
+                    watchlist
+                </Link>
+            </li>}
+            {isLoggedIn && <li className={'col-20'}>
+                <Link className={getColor(newPurchase)} to={newPurchase}>
+                    new purchase
+                </Link>
+            </li>}
+            <li className={'col-20'}>
+                <Link className={getColor(changesInValue)} to={changesInValue}>
+                    Fluctuation
+                </Link>
+            </li>
+            {isLoggedIn && <li className={'col-20'}>
+                <Link className={getColor(favourites)} to={favourites}>
+                    Favourites
+                </Link>
+            </li>}
+        </ul>
+    </div>;
+};
+const Test = (props: any) => {
+    return <div className="container-fluid mobile-overlay w-100 mobile-overlay--up MobileMenuMenu opened">
+        <div className="mobile-overlay__left display-flex flex-column align-items-baseline background-color--black-2 MobileMenuMenu opened">
+            <div className="display-flex align-items-center flex-column">
+                <Link className={getColor(home)} to={home}>
+                    Home
+                </Link>
+                <Link className={getColor(watchlist)} to={watchlist}>
+                    watchlist
+                </Link>
+                <Link className={getColor(newPurchase)} to={newPurchase}>
+                    new purchase
+                </Link>
+                <Link className={getColor(changesInValue)} to={changesInValue}>
+                    Fluctuation
+                </Link>
+                {isLoggedIn && <Link className={getColor(favourites)} to={favourites}>
+                    Favourites
+                </Link>}
+            </div>
+        </div>
+        <div className="mobile-overlay__right display-flex flex-column background-color--mobile-right-1 MobileMenuToggle MobileOpacity display-none opened"/>
+    </div>;
 };
--- src/shared/navigation/mobile-navbar.tsx
+++ src/shared/navigation/mobile-navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { LoginButton } from './login.button';
 import { MobileLinks } from './mobile-links';
 import { MobileMenuButton } from './mobile-menu.button';
@@ -6,15 +6,17 @@ import { MobileMenuButton } from './mobile-menu.button';
 export const MobileNavbar = (props: any) => {
     const [isActive, setIsActive] = useState(false);
 
-    return <nav className="nav-bar position-center">
-        <div className={'row max-width-vw-85 fix-height-50'}>
-            <div className={'col-60'}>
-                <MobileMenuButton isActive={isActive} onClick={() => setIsActive(!isActive)}/>
+    return <Fragment>
+        <nav className="nav-bar position-center">
+            <div className={'row max-width-vw-85 fix-height-50'}>
+                <div className={'col-60'}>
+                    <MobileMenuButton isActive={isActive} onClick={() => setIsActive(!isActive)}/>
+                </div>
+                <div className={'col-40 display-flex justify-content-end'}>
+                    <LoginButton/>
+                </div>
             </div>
-            {isActive && false && <MobileLinks {...props}/>}
-            <div className={'col-40 display-flex justify-content-end'}>
-                <LoginButton/>
-            </div>
-        </div>
-    </nav>;
+        </nav>
+        {true && <MobileLinks {...props}/>}
+    </Fragment>;
 };
